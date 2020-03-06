619cc22c4e81a005551026aad016bd3dc64d10ee
xiaobingsuper <xiaobing.zhang@intel.com>
2020-03-06 16:42:55 +0800

    add cpu running script

---
+++ run_inference_cpu_multi_instance.sh
@@ -0,0 +1,53 @@
+CORES=`lscpu | grep Core | awk '{print $4}'`
+SOCKETS=`lscpu | grep Socket | awk '{print $2}'`
+TOTAL_CORES=`expr $CORES \* $SOCKETS`
+
+# change this number to adjust number of instances
+CORES_PER_INSTANCE=$CORES
+
+
+KMP_SETTING="KMP_AFFINITY=granularity=fine,compact,1,0"
+
+BATCH_SIZE=10
+
+export OMP_NUM_THREADS=$CORES_PER_INSTANCE
+export $KMP_SETTING
+
+echo -e "### using OMP_NUM_THREADS=$CORES_PER_INSTANCE"
+echo -e "### using $KMP_SETTING\n\n"
+sleep 3
+
+INSTANCES=`expr $TOTAL_CORES / $CORES_PER_INSTANCE`
+LAST_INSTANCE=`expr $INSTANCES - 1`
+INSTANCES_PER_SOCKET=`expr $INSTANCES / $SOCKETS`
+for i in $(seq 1 $LAST_INSTANCE); do
+    numa_node_i=`expr $i / $INSTANCES_PER_SOCKET`
+    start_core_i=`expr $i \* $CORES_PER_INSTANCE`
+    end_core_i=`expr $start_core_i + $CORES_PER_INSTANCE - 1`
+    LOG_i=inference_cpu_bs${BATCH_SIZE}_ins${i}.txt
+
+    echo "### running on instance $i, numa node $numa_node_i, core list {$start_core_i, $end_core_i}..."
+    numactl --physcpubind=$start_core_i-$end_core_i --membind=$numa_node_i python main.py -e UCF101 \
+        --batch-size-eval $BATCH_SIZE \
+        --no-cuda \
+        2>&1 | tee $LOG_i &
+done
+
+
+numa_node_0=0
+start_core_0=0
+end_core_0=`expr $CORES_PER_INSTANCE - 1`
+LOG_0=inference_cpu_bs${BATCH_SIZE}_ins0.txt
+
+echo "### running on instance 0, numa node $numa_node_0, core list {$start_core_0, $end_core_0}...\n\n"
+numactl --physcpubind=$start_core_0-$end_core_0 --membind=$numa_node_0 python main.py -e UCF101 \
+    --batch-size-eval $BATCH_SIZE \
+    --no-cuda \
+    2>&1 | tee $LOG_0
+
+sleep 10
+echo -e "\n\n Sum sentences/s together:"
+for i in $(seq 0 $LAST_INSTANCE); do
+    log=inference_cpu_bs${BATCH_SIZE}_ins${i}.txt
+    tail -n 2 $log
+done
